a40428cee9632dec0155fce935f4348e67a148ff
Ivan Tonkov <jmwtpgad56@gmail.com>
2021-03-04 23:31:02 +0300

    First commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public SpriteRenderer playerSprite;
+
+    void start()
+    {
+        playerSprite.flipX = false;
+    }
+
+    public void move(bool flip)
+    {
+        playerSprite.flipX = flip;
+    }
+
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Text Panel_info;
+    public Button Left;
+    public Button Right;
+    //public SpriteRenderer playerSprite;
+    private Player pc;
+    private bool flip = false;
+    //private MoveState _moveState = MoveState.Idle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Panel_info.text = "None";
+        pc = GetComponent<Player>();
+        Left.onClick.AddListener(OnClickLeft);
+        Right.onClick.AddListener(OnClickRight);
+        //playerSprite.flipX = false;
+    }
+
+    public void OnClickLeft()
+    {
+       // Panel_info.text = "Left";
+        flip = true;
+    }
+
+    public void OnClickRight()
+    {
+        //Panel_info.text = "Right";
+        flip = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pc.move(flip);
+    }
+}
